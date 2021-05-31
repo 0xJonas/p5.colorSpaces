c98468d5299cb69e1dc7905d417de362cea95eee
Jonas Rinke <delphi1024@gmail.com>
2021-05-31 22:53:57 +0200

    Implemented CIELab in Rust

---
+++ src/cielab.rs
@@ -0,0 +1,112 @@
+use crate::{TristimulusColor, CIEXYZColor};
+
+use wasm_bindgen::prelude::*;
+
+const CORRECTION_FACTOR_3RD_ROOT: [f32; 5] = [
+    0.6299605249474366,
+    0.7937005259840998,
+    1.0,
+    1.2599210498948732,
+    1.5874010519681994
+];
+
+#[wasm_bindgen]
+#[derive(Debug, Copy, Clone)]
+pub struct CIELabColor(pub f32, pub f32, pub f32);
+
+impl TristimulusColor for CIELabColor {
+
+    fn get_0(&self) -> f32 {
+        self.0
+    }
+
+    fn get_1(&self) -> f32 {
+        self.1
+    }
+
+    fn get_2(&self) -> f32 {
+        self.2
+    }
+}
+
+#[wasm_bindgen]
+pub fn lab_gamma(val: f32) -> f32 {
+    if val <= 0.008856451679035631 {
+        return val / 0.12841854934601665 + 0.13793103448275862;
+    } else {
+        let float_bits: u32 = f32::to_bits(val);
+        let exponent: i32 = ((float_bits >> 23) & 0xff) as i32 - 126;
+        let new_float_bits: u32 = (float_bits & 0x807fffff) | 0x3f000000;
+        let x: f32 = f32::from_bits(new_float_bits);
+
+        let y = ((((x * 0.13380815400574772 - 0.6322612589104776) * 
+                    x + 1.2763453267854687) *
+                        x - 1.4815697155541507) * 
+                            x + 1.327585761559465) * 
+                                x + 0.37609281991938703;
+        
+        let exponent_new = exponent / 3;
+        let correction_shift = f32::from_bits(((exponent_new as u32 + 127) & 0xff) << 23);
+        let correction_index = ((exponent - exponent_new * 3) + 2) as usize;
+        let correction_factor = CORRECTION_FACTOR_3RD_ROOT[correction_index];
+        return y * correction_shift * correction_factor;
+    }
+}
+
+#[wasm_bindgen]
+pub fn lab_digamma(val: f32) -> f32 {
+    if val <= 0.20689655172413793 {
+        (val - 0.13793103448275862) * 0.12841854934601665
+    } else {
+        val * val * val
+    }
+}
+
+#[wasm_bindgen]
+pub fn xyz_to_lab(xyz: CIEXYZColor, white: CIEXYZColor) -> CIELabColor {
+    let CIEXYZColor(x, y, z) = xyz;
+    let CIEXYZColor(xw, yw, zw) = white;
+
+    let x_gamma = lab_gamma(x / xw);
+    let y_gamma = lab_gamma(y / yw);
+    let z_gamma = lab_gamma(z / zw);
+
+    CIELabColor(116.0 * y_gamma - 16.0, 500.0 * (x_gamma - y_gamma), 200.0 * (y_gamma - z_gamma))
+}
+
+#[wasm_bindgen]
+pub fn lab_to_xyz(lab: CIELabColor, white: CIEXYZColor) -> CIEXYZColor {
+    let CIELabColor(l, a, b) = lab;
+    let CIEXYZColor(xw, yw, zw) = white;
+
+    let y_gamma = (l + 16.0) / 116.0;
+    let x_gamma = y_gamma + a / 500.0;
+    let z_gamma = y_gamma - b / 200.0;
+
+    CIEXYZColor(xw * lab_digamma(x_gamma), yw * lab_digamma(y_gamma), zw * lab_digamma(z_gamma))
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::CompareMargin;
+
+    const MARGIN: f32 = 1.0e-3;
+
+    const D65: CIEXYZColor = CIEXYZColor(0.95047, 1.0, 1.08883);
+
+    #[test]
+    fn test_xyz_to_lab() {
+        assert!(xyz_to_lab(CIEXYZColor(0.44272, 0.28123, 0.14037), D65).equal_within(CIELabColor(60.0, 60.0, 30.0), MARGIN));
+        assert!(xyz_to_lab(CIEXYZColor(0.55, 0.9, 0.001), D65).equal_within(CIELabColor(95.997, -66.088, 164.081), MARGIN));
+        assert!(xyz_to_lab(CIEXYZColor(0.3, 0.5, 0.8), D65).equal_within(CIELabColor(76.069, -56.418, -21.731), MARGIN));
+    }
+
+    #[test]
+    fn test_lab_to_xyz() {
+        assert!(lab_to_xyz(CIELabColor(60.0, 60.0, 30.0), D65).equal_within(CIEXYZColor(0.44272, 0.28123, 0.14037), MARGIN));
+        assert!(lab_to_xyz(CIELabColor(95.997, -66.088, 164.081), D65).equal_within(CIEXYZColor(0.55, 0.9, 0.001), MARGIN));
+        assert!(lab_to_xyz(CIELabColor(76.069, -56.418, -21.731), D65).equal_within(CIEXYZColor(0.3, 0.5, 0.8), MARGIN));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 mod rgb;
+mod cielab;
 
 use wasm_bindgen::prelude::*;
 use js_sys::Uint8Array;
 use std::alloc::{alloc, dealloc, Layout};
 
 pub use crate::rgb::*;
+pub use crate::cielab::*;
 
 pub trait TristimulusColor {
     fn get_0(&self) -> f32;
@@ -71,9 +73,9 @@ pub fn convert_memory_srgb_to_xyz(ptr: *mut u8, offset: usize, len: usize) {
         start with the equivalent integer value. Finally, the top 8 bits of the 
         fractional part are extracted to form the final u8 value.
         */
-        data[i * 4 + 0] = (f32::to_bits((x * 200.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
-        data[i * 4 + 1] = (f32::to_bits((y * 200.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
-        data[i * 4 + 2] = (f32::to_bits((z * 200.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 0] = (f32::to_bits((x * 269.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 1] = (f32::to_bits((y * 255.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 2] = (f32::to_bits((z * 235.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
     }
 }
 
@@ -84,9 +86,9 @@ pub fn convert_memory_xyz_to_srgb(ptr: *mut u8, offset: usize, len: usize) {
     };
 
     for i in 0..(data.len() / 4) {
-        let x = data[i * 4 + 0] as f32 / 200.0;
-        let y = data[i * 4 + 1] as f32 / 200.0;
-        let z = data[i * 4 + 2] as f32 / 200.0;
+        let x = data[i * 4 + 0] as f32 / 269.0;
+        let y = data[i * 4 + 1] as f32 / 255.0;
+        let z = data[i * 4 + 2] as f32 / 235.0;
 
         let SRGBColor(r, g, b) = xyz_to_srgb(CIEXYZColor(x, y, z));
 
@@ -109,9 +111,9 @@ pub fn convert_memory_srgb_to_linear_rgb(ptr: *mut u8, offset: usize, len: usize
 
         let LinearRGBColor(lin_r, lin_g, lin_b) = srgb_to_linear_rgb(SRGBColor(r, g, b));
 
-        data[i * 4 + 0] = (f32::to_bits((lin_r * 200.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
-        data[i * 4 + 1] = (f32::to_bits((lin_g * 200.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
-        data[i * 4 + 2] = (f32::to_bits((lin_b * 200.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 0] = (f32::to_bits((lin_r * 255.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 1] = (f32::to_bits((lin_g * 255.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 2] = (f32::to_bits((lin_b * 255.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
     }
 }
 
@@ -128,8 +130,48 @@ pub fn convert_memory_linear_rgb_to_srgb(ptr: *mut u8, offset: usize, len: usize
 
         let SRGBColor(r, g, b) = linear_rgb_to_srgb(LinearRGBColor(lin_r, lin_g, lin_b));
 
-        data[i * 4 + 0] = (f32::to_bits((r * 200.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
-        data[i * 4 + 1] = (f32::to_bits((g * 200.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
-        data[i * 4 + 2] = (f32::to_bits((b * 200.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 0] = (f32::to_bits((r * 255.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 1] = (f32::to_bits((g * 255.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 2] = (f32::to_bits((b * 255.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+    }
+}
+
+#[wasm_bindgen]
+pub fn convert_memory_srgb_to_lab(ptr: *mut u8, offset: usize, len: usize, white: CIEXYZColor) {
+    let data: &mut [u8] = unsafe {
+        std::slice::from_raw_parts_mut(ptr.add(offset), len)
+    };
+
+    for i in 0..(data.len() / 4) {
+        let r = data[i * 4 + 0] as f32 / 255.0;
+        let g = data[i * 4 + 1] as f32 / 255.0;
+        let b = data[i * 4 + 2] as f32 / 255.0;
+
+        let xyz = srgb_to_xyz(SRGBColor(r, g, b));
+        let CIELabColor(l, a_s, b_s) = xyz_to_lab(xyz, white);
+
+        data[i * 4 + 0] = (f32::to_bits((l * 2.55).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 1] = (f32::to_bits((a_s + 128.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 2] = (f32::to_bits((b_s + 128.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+    }
+}
+
+#[wasm_bindgen]
+pub fn convert_memory_lab_to_srgb(ptr: *mut u8, offset: usize, len: usize, white: CIEXYZColor) {
+    let data: &mut [u8] = unsafe {
+        std::slice::from_raw_parts_mut(ptr.add(offset), len)
+    };
+
+    for i in 0..(data.len() / 4) {
+        let l = data[i * 4 + 0] as f32 / 2.550;
+        let a_s = data[i * 4 + 1] as f32 - 128.0;
+        let b_s = data[i * 4 + 2] as f32 - 128.0;
+
+        let xyz = lab_to_xyz(CIELabColor(l, a_s, b_s), white);
+        let SRGBColor(r, g, b) = xyz_to_srgb(xyz);
+
+        data[i * 4 + 0] = (f32::to_bits((r * 255.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 1] = (f32::to_bits((g * 255.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
+        data[i * 4 + 2] = (f32::to_bits((b * 255.0).clamp(0.0, 255.0) + 256.5) >> 15) as u8;
     }
 }
